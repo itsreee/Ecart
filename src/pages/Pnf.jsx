91d49daab930549fc858fe2d0dfd28853cb20943
itsreee <sreelakshmismenon8086@gmail.com>
2024-10-20 11:29:49 +0530

    website

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -5,10 +5,8 @@ const Pnf = () => {
   return (
     <>
         <Header/>
-        <div className="h-80 flex justify-center flex-col items-center" style={{
-          paddingTop:"100px"
-        }}>
-          <img src="" alt="" />
+        <div className="h-80 flex justify-center flex-col items-center" style={{paddingTop:"100px",height:"80vh"}}>
+          <img height={'500px'} width={'300px'} src="https://www.artzstudio.com/content/images/wordpress/2020/05/404-error-not-found-page-lost.png" alt="" />
           <h1 className='text-4xl font-bold '>Page Not Found!</h1>
           <p className='text-xl font-semibold '>Click the Home to redirect to home page</p>
           <Link to={'/'} className='bg-green-600 text-white rounded text-xl font-bold px-2 py-1'>Home</Link>
